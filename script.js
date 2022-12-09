c9b014ce228855eb61aa2714a74a90ebeff62cf1
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-12-09 14:03:35 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 let currentTab = document.getElementById("html-tab");
 
+let code = {
+            html: "", 
+            css: "",
+            js: "",
+            console: "",
+            get htu(){return this.html.slice(this.html.indexOf("<html>")+6, this.html.indexOf("</html>"))}
+           };
+
+
+
 function changeTab(tab)
 {
   if(currentTab.id == tab + "-tab")
@@ -12,4 +22,20 @@ function changeTab(tab)
     currentTab = document.getElementById(tab + "-tab");
     currentTab.style.display = "block";
   }
+  
+  if(currentTab == document.getElementById("page-tab"))
+  {
+    document.getElementById("code").style.display = "none";
+    document.getElementbyId("pageCreated").contentWindow.
+      document.getElementsByTagName("html")[0].innerHTML = code.htu;
+  }
+  else
+  {
+    document.getElementById("code").style.display = "block";
+  }
 }
+
+document.getElementById("code").onclick = function(){
+
+  document.getElementById(currentTab.id != "console-tab" && currentTab.id != "page-tab" ? currentTab.id.slice(0, -4) : currentTab.id == "consoleTab" ? "console" : "i am under the water").focus();
+};
