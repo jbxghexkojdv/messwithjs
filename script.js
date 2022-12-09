f43506e1cb34eaf6cccda6822a7c71f4549159eb
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-12-09 13:11:31 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+let currentTab = document.getElementById("html-tab");
 
+function changeTab(tab)
+{
+  if(currentTab.id == tab + "-tab")
+  {
+    return;
+  }
+  else
+  {
+    currentTab.style.display = "none";
+    currentTab = document.getElementById(tab + "-tab");
+    currentTab.style.display = "block";
+  }
+}
